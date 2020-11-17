56282eee98d38a30226ba419ff55aeca8af36140
williams335 <doodzerj@gmail.com>
2020-11-17 02:32:24 +0100

    SplashScreen

--- app/src/main/java/com/example/snowtam_kk/View/MainActivity.java
+++ app/src/main/java/com/example/snowtam_kk/View/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.snowtam_kk;
+package com.example.snowtam_kk.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.snowtam_kk.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/snowtam_kk/View/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.snowtam_kk.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.snowtam_kk.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.LightTheme);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Rediriger vers la MainACtivity après 4 secondes
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+    }
+}
